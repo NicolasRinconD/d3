f0e9657d8a079b65ee7e04435958c2b1886381e9
NicolasRinconD <n.rincond@uniandes.edu.co>
2020-05-12 00:50:54 -0500

    BARS

--- reto1.js
+++ reto1.js
@@ -1,52 +1,54 @@
-const canvas = d3.select("#canvas");
-
-const width = 700;
-const height = 600;
-const margin = { top: 10, left: 80, bottom: 0, right: 10 };
-const iwidth = width - margin.left - margin.right;
-const iheight = height - margin.top - margin.bottom;
-
-const svg = canvas.append("svg");
-svg.attr("width", width);
-svg.attr("height", height);
-
-let g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
-
-var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-    y = d3.scaleLinear().rangeRound([height, 0]);
 
 
 
-d3.json("https://gist.githubusercontent.com/josejbocanegra/d3b9e9775ec3a646603f49bc8d3fb90f/raw/3a39300c2a2ff8644a52e22228e900251ec5880a/population.json").then((data) => {
-    console.log(data);
-    x.domain(data.map(function(d) { return d.name; }));
-    y.domain([0, d3.max(data, function(d) { return d.value; })]);
+d3.json("https://gist.githubusercontent.com/josejbocanegra/d3b9e9775ec3a646603f49bc8d3fb90f/raw/3a39300c2a2ff8644a52e22228e900251ec5880a/population.json")
+.then((data) => {
+  const canvas = d3.select("#canvas");
 
-    g.append("g")
-        .attr("class", "axis axis--x")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(y));
+  const width = 700;
+  const height = 600;
+  const margin = { top: 10, left: 80, bottom: 30, right: 10 };
+  const iwidth = width - margin.left - margin.right;
+  const iheight = height - margin.top - margin.bottom;
 
-    g.append("g")
-        .attr("class", "axis axis--y")
-        .call(d3.axisLeft(x))
-      .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "0.71em")
-        .attr("text-anchor", "end")
-        .text("value");
+  const svg = canvas.append("svg");
+  svg.attr("width", width);
+  svg.attr("height", height);
 
-    g.selectAll(".bar")
-      .data(data)
-      .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return x(d.value); })
-        .attr("y", function(d) { return y(d.name); })
-        .attr("width", x.bandwidth())
-        .attr("height", function(d) { return height - y(d.value); });
+  data.forEach(function(d) {
+    d.value = +d.value;
+  });
+
+  let g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
+
+  var x = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.value)])
+    .range([0, iwidth]);
+
+  var y = d3.scaleBand()
+    .domain(data.map(function(d) { return d.name; }))
+    .range([iheight, 0])
+    .padding(0.1);
+
+  g.append("g")
+    .attr("class", "axis axis--x")
+    .attr("transform", "translate(0," + iheight + ")")
+    .call(d3.axisBottom(x));
+
+  g.append("g")
+    .attr("class", "axis axis--y")
+    .call(d3.axisLeft(y))
+
+  g.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+    .attr("class", "bar")
+    //.attr("x", function (d) { return x(d.value); })
+    .attr("y", function (d) { return y(d.name); })
+    .attr("width", function (d) { return x(d.value); })
+    .attr("height", y.bandwidth());
 })
-.catch((error) => {
-        throw error;
-});
+  .catch((error) => {
+    throw error;
+  });
 
