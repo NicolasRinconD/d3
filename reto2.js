b923ec773801a579225e99bc517eb602fe7a0b84
NicolasRinconD <n.rincond@uniandes.edu.co>
2020-05-12 00:12:10 -0500

    circles

--- reto2.js
+++ reto2.js
@@ -1,38 +1,49 @@
+const canvas = d3.select("#canvas");
 
-
-d3.json("https://gist.githubusercontent.com/josejbocanegra/000e838b77c6ec8e5d5792229c1cdbd0/raw/83cd9161e28e308ef8c5363e217bad2b6166f21a/countries.json")
-.then(data =>{
+d3.json("https://gist.githubusercontent.com/josejbocanegra/000e838b77c6ec8e5d5792229c1cdbd0/raw/83cd9161e28e308ef8c5363e217bad2b6166f21a/countries.json").then(data=>{
     return data.map((d) => {
-        d.purchasingpower = +d.purchasingpower;
-        d.lifeexpectancy = +d.lifeexpectancy;
         d.population = +d.population;
-        return d;
-    }); 
-})
-.then((data) => {
-    var svg = d3.select("#canvas")
-  .append("svg")
-  .attr("width", 300)
-  .attr("height", 200);
-
-var xScale = d3.scaleLinear()
-  .domain([0, d3.max(data, function(d) { return d.purchasingpower; })])
-  .range([0, 300]);
-
-var barWidth = xScale.domain()[1]/data.length;
-
-var yScale = d3.scaleLinear()
-  .domain([0, d3.max(data, function(d) { return d.lifeexpectancy; })])
-  .range([200, 0]);
-
-var bars = svg.selectAll(".bars")
-  .data(data)
-  .enter()
-  .append("rect");
-
-bars.attr("x", function(d){ return xScale(d.purchasingpower)})
-  .attr("y", function(d){ return yScale(d.lifeexpectancy)})
-  .attr("width", function(d,i){ return xScale(barWidth) * 0.9})
-  .attr("height", function(d){ return 200 - yScale(d.lifeexpectancy)});
-
+        d.lifeexpectancy = +d.lifeexpectancy;
+        d.purchasingpower = +d.purchasingpower;
+        return d;  
+      });
+}).then(data=>{
+    
+    const width = 700;
+    const height = 500;
+    const margin = { top:10, left:50, bottom: 40, right: 10};
+    const iwidth = width - margin.left - margin.right;
+    const iheight = height - margin.top -margin.bottom;
+    
+    const svg = canvas.append("svg");
+    svg.attr("width", width);
+    svg.attr("height", height);
+    
+    let g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
+    
+    const y = d3.scaleLinear() 
+        .domain([0, d3.max(data, d => d.lifeexpectancy)])
+        .range([iheight, 0]);
+    
+    const x = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.purchasingpower)]) 
+    .range([0, iwidth]); 
+    
+    const circles = g.selectAll("rect").data(data);
+    
+    circles.enter().append('circle')
+    .attr('cy', d => y(d.lifeexpectancy))
+    .attr('cx', d => x(d.purchasingpower))
+    .attr('r', d => d.population/1590000); 
+    
+    g.append("g")
+    .classed("x--axis", true)
+    .call(d3.axisBottom(x))
+    .attr("transform", `translate(0, ${iheight})`);  
+    
+    g.append("g")
+    .classed("y--axis", true)
+    .call(d3.axisLeft(y));
 });
+
+
